eb8ca05591e78cd6b9a5969c64725ce7b3a31f16
MatiasCS1997 <mcorderos@miucsh.cl>
2022-09-05 15:42:50 -0400

    players & formulary

---
+++ src/components/Collaborators.jsx
@@ -0,0 +1,22 @@
+export const CollaboratorsBase = [
+  {
+    key: "1",
+    nombre: "Waffles",
+    correo: "Waffles@gmail.cl",
+  },
+  {
+    key: "2",
+    nombre: "Lil Nachito",
+    correo: "lilnachito@gmail.cl",
+  },
+  {
+    key: "3",
+    nombre: "Zepekenho",
+    correo: "zepekenho@gmail.cl",
+  },
+  {
+    key: "4",
+    nombre: "Matie",
+    correo: "Matie182@gmail.cl",
+  },
+];
---
+++ src/components/Formulary.jsx
@@ -0,0 +1,81 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
+import { CollaboratorsBase } from "../components/Collaborators";
+import Search from "../components/Search";
+
+const Formulary = () => {
+  const [user, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [users, setUsers] = useState([]);
+  const [task, setTask] = useState(CollaboratorsBase);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!user) {
+      alert("Debes completar los campos vacíos");
+      return;
+    }
+
+    setTask([...task, { id: nanoid(), nombre: user, correo: email }]);
+    setName("");
+    setEmail("");
+  };
+
+  return (
+    <>
+      <div id="search">
+        <Search tasks={task} setTask={setTask} />
+      </div>
+      <section>
+        <div id="add">
+          <h3>Nombre</h3>
+          <form onSubmit={handleSubmit}>
+            <input
+              type="text"
+              className="Seeker"
+              placeholder="Ingrese nombre"
+              onChange={(e) => setName(e.target.value)}
+              value={user}
+            />
+            <h3>Email</h3>
+            <input
+              type="text"
+              className="Seeker"
+              placeholder="Ingrese correo"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <button type="submit">Enviar</button>
+          </form>
+        </div>
+      </section>
+
+      <div id="users">
+        <h3>Jugadores encontrados</h3>
+        <table>
+          <thead>
+            <tr>
+              <th>Nombre</th>
+              <th>Correo</th>
+            </tr>
+          </thead>
+          <tbody>
+            {task.map((el) => (
+              <tr key={el.id}>
+                <td>{el.nombre}</td>
+                <td>{el.correo}</td>
+              </tr>
+            ))}
+            {users.map((item) => (
+              <tr key={item.id}>
+                <td>{item.nombre}</td>
+                <td>{item.correo}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </>
+  );
+};
+export default Formulary;
